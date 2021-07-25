6e7e1d0ee329e3f9239f8a5dfee0d3bf2d4a02b9
yuya <yuyawamo@gmail.com>
2021-07-25 11:37:36 +0200

    updates app.py

--- yt-profile/flask-app/app.py
+++ yt-profile/flask-app/app.py
@@ -11,5 +11,6 @@ def salvador():
     return render_template("project.html")
             
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
     #app.run(host="34.141.68.174")
