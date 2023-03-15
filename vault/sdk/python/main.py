76fb1dd96fe6ef82338373da9f7ab91f7819d046
yuyatinnefeld <yuyawamo@gmail.com>
2023-03-15 19:34:23 +0100

    feat: update python sdk

--- vault/sdk/python/main.py
+++ vault/sdk/python/main.py
@@ -7,20 +7,21 @@ client = hvac.Client(
     token='dev-only-token',
 )
 
-# Writing a secret
-create_response = client.secrets.kv.v2.create_or_update_secret(
-    path='my-secret-password',
-    secret=dict(password='Hashi123'),
-)
+label="password_hashicorp"
 
-print('Secret written successfully.')
+# Writing a secret
+for x in range(6):
+    create_response = client.secrets.kv.v2.create_or_update_secret(
+        path=f'my-secret-password-{x}',
+        secret=dict(password=f'{label}_{x}'),
+    )
+
+print("Secret written successfully.\n")
 
 # Reading a secret
-read_response = client.secrets.kv.read_secret_version(path='my-secret-password')
-
-password = read_response['data']['data']['password']
-
-if password != 'Hashi123':
-    sys.exit('unexpected password')
-
-print('Access granted!')
+for x in range(6):
+    scret_num = f"SECRET {x}"
+    print(scret_num.center(100, "*"),"\n")
+    read_response = client.secrets.kv.read_secret_version(path=f'my-secret-password-{x}')
+    password = read_response['data']['data']['password']
+    print(f"SECRET_NAME: my-secret-password-{x} | PASSWORD: {password} \n")
